0fca45f066e53b1836450b60584c78a6d0455c3a
Jermin Vasquez <jerminshadin@gmail.com>
2025-05-08 09:19:04 -0500

    Versión inicial del proyecto La Hueca del Sabor

---
+++ frontend/src/roles/mesero/DashboardMesero.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const DashboardMesero = () => {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Panel del Mesero</h1>
+      <p>Aquí podrás:</p>
+      <ul>
+        <li>Tomar pedidos</li>
+        <li>Consultar menú disponible</li>
+        <li>Enviar orden a cocina y caja</li>
+      </ul>
+    </div>
+  );
+};
+
+export default DashboardMesero;
